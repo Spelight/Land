9e2ef2b1a17eaf1344c8b239910fef0abddf33b9
HXS__ <112705581+FoxLlN@users.noreply.github.com>
2024-08-14 14:52:07 +1000

    monster

--- src/main/kotlin/com/mcstarrysky/land/flag/PermDamageMonster.kt
+++ src/main/kotlin/com/mcstarrysky/land/flag/PermDamageMonster.kt
@@ -5,7 +5,7 @@ import com.mcstarrysky.land.manager.LandManager
 import com.mcstarrysky.land.util.display
 import com.mcstarrysky.land.util.prettyInfo
 import com.mcstarrysky.land.util.registerPermission
-import org.bukkit.entity.Golem
+import org.bukkit.entity.Monster
 import org.bukkit.entity.Player
 import org.bukkit.event.entity.EntityDamageByEntityEvent
 import org.bukkit.inventory.ItemFlag
@@ -18,12 +18,12 @@ import taboolib.platform.util.buildItem
 
 /**
  * Land
- * com.mcstarrysky.land.flag.PermDamageGolem
+ * com.mcstarrysky.land.flag.PermDamageMonster
  *
  * @author HXS
- * @since 2024/8/14 13:57
+ * @since 2024/8/14 14:49
  */
-object PermDamageGolem : Permission {
+object PermDamageMonster : Permission{
 
     @Awake(LifeCycle.ENABLE)
     private fun init() {
@@ -31,7 +31,7 @@ object PermDamageGolem : Permission {
     }
 
     override val id: String
-        get() = "damage_golem"
+        get() = "damage_monster"
 
     override val default: Boolean
         get() = false
@@ -42,12 +42,13 @@ object PermDamageGolem : Permission {
     override val playerSide: Boolean
         get() = true
 
-    override fun generateMenuItem(land: Land): ItemStack {
-        return buildItem(XMaterial.STONE_SWORD){
-            name = "&f攻击傀儡 ${land.getFlagOrNull(id).display}"
-            lore += listOf(
+        override fun generateMenuItem(land: Land): ItemStack {
+            return buildItem(XMaterial.GOLDEN_SWORD){
+                name = "&f攻击怪物 ${land.getFlagOrNull(id).display}"
+                lore += listOf(
+                "",
                 "&7允许行为:",
-                "&8对傀儡 (Golem) 造成伤害",
+                "&8对怪物 (Monster) 造成伤害",
                 "",
                 "&e左键修改值, 右键取消设置"
             )
@@ -59,12 +60,12 @@ object PermDamageGolem : Permission {
 
     @SubscribeEvent(ignoreCancelled = true)
     fun e(e: EntityDamageByEntityEvent) {
-        if (e.entity is Golem) {
-            val player = e.damager as? Player ?: return
+        if (e.entity is Monster) {
+            val player = e.damager as Player
             LandManager.getLand(e.entity.location)?.run {
-                if (!hasPermission(player) && !getFlag(this@PermDamageGolem.id)) {
+                if (!hasPermission(player) && !getFlag(this@PermDamageMonster.id)) {
                     e.isCancelled = true
-                    player.prettyInfo("没有权限, 禁止攻击傀儡&7\\(标记: ${this@PermDamageGolem.id}\\)")
+                    player.prettyInfo("没有权限, 禁止攻击怪物&7\\(标记: ${this@PermDamageMonster.id}\\)")
                 }
             }
         }
